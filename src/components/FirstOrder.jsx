e38389461e040f81c108e055d60052a096039d55
mohamed2233996 <medomano771@gmail.com>
2026-01-03 12:36:37 +0200

    fix: Adjust button styles in First Order component for consistency

--- src/components/FirstOrder.jsx
+++ src/components/FirstOrder.jsx
@@ -19,7 +19,7 @@ const FirstOrder = () => {
                 <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white z-30">
                     <h2 className="text-4xl md:text-5xl font-bold mb-4">{t("firstOrder")}</h2>
                     <p className="text-lg md:text-xl mb-12">{t("Use_code")} <span className="font-mono bg-white text-black px-2 py-1 rounded">FIRST10</span>{t("at_checkout")}.</p>
-                    <a href="#shop" className="inline-block bg-white text-black text-xl font-black shadow-[0px_0px_10px_0px_#fc8c06,0px_0px_20px_0px_#fc8c06,0px_0px_30px_0px_#fc8c06,0px_0px_40px_0px_#fc8c06,0px_0px_50px_0px_#fc8c06,0px_0px_60px_0px_#fc8c06,0px_0px_70px_0px_#fc8c06] px-6 py-3 rounded-4xl hover:scale-105 transition">
+                    <a href="#shop" className="inline-block bg-white text-black text-lg leading-normal font-black shadow-[0px_0px_10px_0px_#fc8c06,0px_0px_20px_0px_#fc8c06,0px_0px_30px_0px_#fc8c06,0px_0px_40px_0px_#fc8c06,0px_0px_50px_0px_#fc8c06,0px_0px_60px_0px_#fc8c06,0px_0px_70px_0px_#fc8c06] px-4 py-2 rounded-4xl hover:scale-105 transition">
                         {t("Shop_Now")}
                     </a>
                 </div>
